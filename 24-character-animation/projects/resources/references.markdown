81c58011f23f006f1c32711a629f8106685ee198
edith <edith@razeware.com>
2021-11-18 01:28:57 +0000

    Renaming as per proposal

---
+++ 24-character-animation/projects/resources/references.markdown
@@ -0,0 +1,46 @@
+## Animation general
+
+[Pixar in a box](https://www.pixar.com/pixar-in-a-box)
+
+### 12 Principles of Animation
+
+[The Illusion of Life: Disney Animation by Ollie Johnston and Frank Thomas](http://a.co/aQdw3AS)    
+[Understand the 12 principles of animation](https://www.creativebloq.com/advice/understand-the-12-principles-of-animation)    
+[The 12 Principles of Animation (Pixar-ated)](https://vimeo.com/207384652)
+
+## Blender animation 
+
+(Unfortunately version 2.79. The concepts are the same, but the interface will be different.)
+
+[Animate a mushroom](https://www.raywenderlich.com/52804/beginning-blender-tutorial-animating-rendering-mushroom)    
+[Sebastian Lague videos](https://www.youtube.com/playlist?list=PLFt_AvWsXl0fEx02iXR8uhDsVGhmM9Pse)    
+[Grant Abbitt](https://www.youtube.com/playlist?list=PLn3ukorJv4vvNIeMqu82PqM37Xnds5zAy)
+
+## Procedural animation
+
+[Better Explained: Intuitive Understanding of Sine Waves](https://betterexplained.com/articles/intuitive-understanding-of-sine-waves/)    
+[The Nature of Code by Daniel Shiffman](http://natureofcode.com/book/chapter-3-oscillation/)
+
+## Sample models
+
+[https://sketchfab.com](https://sketchfab.com)
+
+## USD
+
+[Pixar's USD documenation](https://graphics.pixar.com/usd/docs/index.html)
+[Apple's USDZ tools](https://developer.apple.com/augmented-reality/quick-look/)
+
+## Quaternions
+
+[Apple on quaternions](https://developer.apple.com/videos/play/wwdc2017-711/?time=1005)    
+[Apple documentation](https://developer.apple.com/documentation/accelerate/simd/working_with_quaternions)    
+[Performance of quaternions in the GPU](http://tech.metail.com/performance-quaternions-gpu/)    
+[Letter from Sir William R. Hamilton](https://www.maths.tcd.ie/pub/HistMath/People/Hamilton/QLetter/QLetter.pdf)    
+[Quaternions](http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/)
+
+## Gimbal Lock
+
+[Gimbal Lock Explained in 30 Seconds](https://youtu.be/N5PDboNJwks)
+
+
+
---
+++ 24-character-animation/projects/starter/Animation/Animation/Submesh.swift
@@ -0,0 +1,162 @@
+//
+/**
+ * Copyright (c) 2019 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import MetalKit
+
+class Submesh {
+  var mtkSubmesh: MTKSubmesh
+  
+  struct Textures {
+    let baseColor: MTLTexture?
+    let normal: MTLTexture?
+    let roughness: MTLTexture?
+    let metallic: MTLTexture?
+    let ao: MTLTexture?
+  }
+  
+  let textures: Textures
+  let material: Material
+  let pipelineState: MTLRenderPipelineState
+  
+  init(mdlSubmesh: MDLSubmesh, mtkSubmesh: MTKSubmesh) {
+    self.mtkSubmesh = mtkSubmesh
+    textures = Textures(material: mdlSubmesh.material)
+    material = Material(material: mdlSubmesh.material)
+    pipelineState = Submesh.makePipelineState(textures: textures)
+  }
+}
+
+// Pipeline state
+private extension Submesh {
+  static func makeFunctionConstants(textures: Textures)
+    -> MTLFunctionConstantValues {
+      let functionConstants = MTLFunctionConstantValues()
+      var property = textures.baseColor != nil
+      functionConstants.setConstantValue(&property, type: .bool, index: 0)
+      property = textures.normal != nil
+      functionConstants.setConstantValue(&property, type: .bool, index: 1)
+      property = textures.roughness != nil
+      functionConstants.setConstantValue(&property, type: .bool, index: 2)
+      property = textures.metallic != nil
+      functionConstants.setConstantValue(&property, type: .bool, index: 3)
+      property = textures.ao != nil
+      functionConstants.setConstantValue(&property, type: .bool, index: 4)
+      return functionConstants
+  }
+
+  static func makeVertexFunctionConstants(hasSkeleton: Bool) -> MTLFunctionConstantValues {
+    let functionConstants = MTLFunctionConstantValues()
+    var addSkeleton = hasSkeleton
+    functionConstants.setConstantValue(&addSkeleton, type: .bool, index: 5)
+    return functionConstants
+  }
+
+  static func makePipelineState(textures: Textures) -> MTLRenderPipelineState {
+    let functionConstants = makeFunctionConstants(textures: textures)
+    
+    let library = Renderer.library
+    let vertexFunction = library?.makeFunction(name: "vertex_main")
+    let fragmentFunction: MTLFunction?
+    do {
+      fragmentFunction = try library?.makeFunction(name: "fragment_mainPBR",
+                                                   constantValues: functionConstants)
+    } catch {
+      fatalError("No Metal function exists")
+    }
+    
+    var pipelineState: MTLRenderPipelineState
+    let pipelineDescriptor = MTLRenderPipelineDescriptor()
+    pipelineDescriptor.vertexFunction = vertexFunction
+    pipelineDescriptor.fragmentFunction = fragmentFunction
+    
+    let vertexDescriptor = Model.vertexDescriptor
+    pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(vertexDescriptor)
+    pipelineDescriptor.colorAttachments[0].pixelFormat = Renderer.colorPixelFormat
+    pipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
+    do {
+      pipelineState = try Renderer.device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+    } catch let error {
+      fatalError(error.localizedDescription)
+    }
+    return pipelineState
+  }
+}
+
+
+extension Submesh: Texturable {}
+
+private extension Submesh.Textures {
+  init(material: MDLMaterial?) {
+    func property(with semantic: MDLMaterialSemantic) -> MTLTexture? {
+      guard let property = material?.property(with: semantic),
+        property.type == .string,
+        let filename = property.stringValue,
+        let texture = try? Submesh.loadTexture(imageName: filename)
+        else {
+          if let property = material?.property(with: semantic),
+            property.type == .texture,
+            let mdlTexture = property.textureSamplerValue?.texture {
+            return try? Submesh.loadTexture(texture: mdlTexture)
+          }
+          return nil
+      }
+      return texture
+    }
+    baseColor = property(with: MDLMaterialSemantic.baseColor)
+    normal = property(with: .tangentSpaceNormal)
+    roughness = property(with: .roughness)
+    metallic = property(with: .metallic)
+    ao = property(with: .ambientOcclusion)
+  }
+}
+
+private extension Material {
+  init(material: MDLMaterial?) {
+    self.init()
+    if let baseColor = material?.property(with: .baseColor),
+      baseColor.type == .float3 {
+      self.baseColor = baseColor.float3Value
+    }
+    if let specular = material?.property(with: .specular),
+      specular.type == .float3 {
+      self.specularColor = specular.float3Value
+    }
+    if let shininess = material?.property(with: .specularExponent),
+      shininess.type == .float {
+      self.shininess = shininess.floatValue
+    }
+    if let roughness = material?.property(with: .roughness),
+      roughness.type == .float3 {
+      self.roughness = roughness.floatValue
+    }
+  }
+}
+
