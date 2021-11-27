6497417623cc5f5c61f76531dcd56229132814cc
Caroline Begbie <c.b100@aol.com>
2021-11-27 13:57:27 +1000

    Chapter 5 author complete

---
+++ 05-3d-transformations/projects/starter/Spaces/Shared/Renderer.swift
@@ -0,0 +1,154 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import MetalKit
+
+// swiftlint:disable implicitly_unwrapped_optional
+
+class Renderer: NSObject {
+  static var device: MTLDevice!
+  static var commandQueue: MTLCommandQueue!
+  static var library: MTLLibrary!
+  var pipelineState: MTLRenderPipelineState!
+
+  lazy var triangle: Triangle = {
+    Triangle(device: Renderer.device)
+  }()
+
+  init(metalView: MTKView) {
+    guard
+      let device = MTLCreateSystemDefaultDevice(),
+      let commandQueue = device.makeCommandQueue() else {
+        fatalError("GPU not available")
+    }
+    Renderer.device = device
+    Renderer.commandQueue = commandQueue
+    metalView.device = device
+
+    // create the shader function library
+    let library = device.makeDefaultLibrary()
+    Renderer.library = library
+    let vertexFunction = library?.makeFunction(name: "vertex_main")
+    let fragmentFunction =
+      library?.makeFunction(name: "fragment_main")
+
+    // create the pipeline state
+    let pipelineDescriptor = MTLRenderPipelineDescriptor()
+    pipelineDescriptor.vertexFunction = vertexFunction
+    pipelineDescriptor.fragmentFunction = fragmentFunction
+    pipelineDescriptor.colorAttachments[0].pixelFormat =
+      metalView.colorPixelFormat
+    pipelineDescriptor.vertexDescriptor =
+      MTLVertexDescriptor.defaultLayout
+    do {
+      pipelineState =
+        try device.makeRenderPipelineState(
+          descriptor: pipelineDescriptor)
+    } catch let error {
+      fatalError(error.localizedDescription)
+    }
+    super.init()
+    metalView.clearColor = MTLClearColor(
+      red: 1.0,
+      green: 1.0,
+      blue: 0.9,
+      alpha: 1.0)
+    metalView.delegate = self
+  }
+}
+
+extension Renderer: MTKViewDelegate {
+  func mtkView(
+    _ view: MTKView,
+    drawableSizeWillChange size: CGSize
+  ) { }
+
+  func draw(in view: MTKView) {
+    guard
+      let commandBuffer = Renderer.commandQueue.makeCommandBuffer(),
+      let descriptor = view.currentRenderPassDescriptor,
+      let renderEncoder =
+        commandBuffer.makeRenderCommandEncoder(
+          descriptor: descriptor) else {
+        return
+    }
+
+    renderEncoder.setRenderPipelineState(pipelineState)
+    renderEncoder.setVertexBuffer(
+      triangle.vertexBuffer,
+      offset: 0,
+      index: 0)
+
+    // draw the untransformed triangle in light gray
+    var color: simd_float4 = [0.8, 0.8, 0.8, 1]
+    renderEncoder.setFragmentBytes(
+      &color,
+      length: MemoryLayout<SIMD4<Float>>.stride,
+      index: 0)
+    var position = simd_float3(0, 0, 0)
+    renderEncoder.setVertexBytes(
+      &position,
+      length: MemoryLayout<SIMD3<Float>>.stride,
+      index: 11)
+    renderEncoder.drawIndexedPrimitives(
+      type: .triangle,
+      indexCount: triangle.indices.count,
+      indexType: .uint16,
+      indexBuffer: triangle.indexBuffer,
+      indexBufferOffset: 0)
+
+    // draw the new triangle in red
+    color = [1, 0, 0, 1]
+    renderEncoder.setFragmentBytes(
+      &color,
+      length: MemoryLayout<SIMD4<Float>>.stride,
+      index: 0)
+    position = simd_float3(0.3, -0.4, 0)
+    renderEncoder.setVertexBytes(
+      &position,
+      length: MemoryLayout<SIMD3<Float>>.stride,
+      index: 11)
+    renderEncoder.drawIndexedPrimitives(
+      type: .triangle,
+      indexCount: triangle.indices.count,
+      indexType: .uint16,
+      indexBuffer: triangle.indexBuffer,
+      indexBufferOffset: 0)
+
+    renderEncoder.endEncoding()
+    guard let drawable = view.currentDrawable else {
+      return
+    }
+    commandBuffer.present(drawable)
+    commandBuffer.commit()
+  }
+}
---
+++ 05-3d-transformations/projects/starter/Spaces/Shared/Triangle.swift
@@ -0,0 +1,69 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import MetalKit
+
+// swiftlint:disable collection_alignment
+// swiftlint:disable comma
+// swiftlint:disable indentation_width
+
+struct Triangle {
+  var vertices: [Float] = [
+    -0.7,  0.8,  0,
+    -0.7, -0.5,  0,
+     0.4,  0.1,  0
+  ]
+
+  var indices: [UInt16] = [
+    0, 1, 2
+  ]
+
+  let vertexBuffer: MTLBuffer
+  let indexBuffer: MTLBuffer
+
+  init(device: MTLDevice) {
+    guard let vertexBuffer = device.makeBuffer(
+      bytes: &vertices,
+      length: MemoryLayout<Float>.stride * vertices.count,
+      options: []) else {
+      fatalError("Unable to create quad vertex buffer")
+    }
+    self.vertexBuffer = vertexBuffer
+    guard let indexBuffer = device.makeBuffer(
+      bytes: &indices,
+      length: MemoryLayout<UInt16>.stride * indices.count,
+      options: []) else {
+      fatalError("Unable to create quad vertex buffer")
+    }
+    self.indexBuffer = indexBuffer
+  }
+}
