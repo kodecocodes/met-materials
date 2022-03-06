1bda57195b61c86d3808de3dd4b1d5979ac137e2
Caroline Begbie <c.b100@aol.com>
2022-03-06 14:03:18 +1000

    Chapter 10 update license date and input

--- 10-lighting-fundamentals/projects/final/Lighting/Shared/SwiftUI Views/MetalView.swift
+++ 10-lighting-fundamentals/projects/final/Lighting/Shared/SwiftUI Views/MetalView.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2022 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -37,6 +37,8 @@ struct MetalView: View {
   let options: Options
   @State private var metalView = MTKView()
   @State private var gameController: GameController?
+  @State private var previousTranslation = CGSize.zero
+  @State private var previousScroll: CGFloat = 1
 
   var body: some View {
     VStack {
@@ -49,6 +51,32 @@ struct MetalView: View {
             metalView: metalView,
             options: options)
         }
+        .gesture(DragGesture(minimumDistance: 0)
+        .onChanged { value in
+          InputController.shared.touchLocation = value.location
+          InputController.shared.touchDelta = CGSize(
+            width: value.translation.width - previousTranslation.width,
+            height: value.translation.height - previousTranslation.height)
+          previousTranslation = value.translation
+          // if the user drags, cancel the tap touch
+          if abs(value.translation.width) > 1 ||
+            abs(value.translation.height) > 1 {
+            InputController.shared.touchLocation = nil
+          }
+        }
+        .onEnded {_ in
+          previousTranslation = .zero
+        })
+      .gesture(MagnificationGesture()
+        .onChanged { value in
+          let scroll = value - previousScroll
+          InputController.shared.mouseScroll.x = Float(scroll)
+            * Settings.touchZoomSensitivity
+          previousScroll = value
+        }
+        .onEnded {_ in
+          previousScroll = 1
+        })
     }
   }
 }
