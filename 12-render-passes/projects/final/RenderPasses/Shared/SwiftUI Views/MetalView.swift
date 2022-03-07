6f7b84ce1fa9abc89a76cc789f5216b37b59833d
Caroline Begbie <c.b100@aol.com>
2022-03-07 12:47:04 +1000

    Chapter 12 iOS input

--- 12-render-passes/projects/final/RenderPasses/Shared/SwiftUI Views/MetalView.swift
+++ 12-render-passes/projects/final/RenderPasses/Shared/SwiftUI Views/MetalView.swift
@@ -37,6 +37,8 @@ struct MetalView: View {
   let options: Options
   @State private var metalView = MTKView()
   @State private var gameController: GameController?
+  @State private var previousTranslation = CGSize.zero
+  @State private var previousScroll: CGFloat = 1
 
   var body: some View {
     VStack {
@@ -50,14 +52,31 @@ struct MetalView: View {
             options: options)
         }
         .gesture(DragGesture(minimumDistance: 0)
-          .onChanged { value in
-            InputController.shared.touchLocation = value.location
-            // if the user drags, cancel the tap touch
-            if abs(value.translation.width) > 1 ||
-              abs(value.translation.height) > 1 {
-              InputController.shared.touchLocation = nil
-            }
-          })
+        .onChanged { value in
+          InputController.shared.touchLocation = value.location
+          InputController.shared.touchDelta = CGSize(
+            width: value.translation.width - previousTranslation.width,
+            height: value.translation.height - previousTranslation.height)
+          previousTranslation = value.translation
+          // if the user drags, cancel the tap touch
+          if abs(value.translation.width) > 1 ||
+            abs(value.translation.height) > 1 {
+            InputController.shared.touchLocation = nil
+          }
+        }
+        .onEnded {_ in
+          previousTranslation = .zero
+        })
+        .gesture(MagnificationGesture()
+        .onChanged { value in
+          let scroll = value - previousScroll
+          InputController.shared.mouseScroll.x = Float(scroll)
+            * Settings.touchZoomSensitivity
+          previousScroll = value
+        }
+        .onEnded {_ in
+          previousScroll = 1
+        })
     }
   }
 }
--- 12-render-passes/projects/starter/RenderPasses/Shared/Input/Movement.swift
+++ 12-render-passes/projects/starter/RenderPasses/Shared/Input/Movement.swift
@@ -37,6 +37,7 @@ enum Settings {
   static var translationSpeed: Float { 3.0 }
   static var mouseScrollSensitivity: Float { 0.1 }
   static var mousePanSensitivity: Float { 0.008 }
+  static var touchZoomSensitivity: Float { 10 }
 }
 
 protocol Movement where Self: Transformable {
