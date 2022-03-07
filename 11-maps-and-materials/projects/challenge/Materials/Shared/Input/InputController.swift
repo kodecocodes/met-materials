6f26fed47527f8bb93b54d2e761430c3171762df
Caroline Begbie <c.b100@aol.com>
2022-03-07 10:34:45 +1000

    Chapter 11 update license dates and iOS input

--- 11-maps-and-materials/projects/challenge/Materials/Shared/Input/InputController.swift
+++ 11-maps-and-materials/projects/challenge/Materials/Shared/Input/InputController.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
+/// Copyright (c) 2022 Razeware LLC
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -44,6 +44,16 @@ class InputController {
   var leftMouseDown = false
   var mouseDelta = Point.zero
   var mouseScroll = Point.zero
+  var touchLocation: CGPoint?
+  var touchDelta: CGSize? {
+    didSet {
+      touchDelta?.height *= -1
+      if let delta = touchDelta {
+        mouseDelta = Point(x: Float(delta.width), y: Float(delta.height))
+      }
+      leftMouseDown = touchDelta != nil
+    }
+  }
 
   private init() {
     let center = NotificationCenter.default
@@ -66,15 +76,9 @@ class InputController {
       object: nil,
       queue: nil) { notification in
         let mouse = notification.object as? GCMouse
-        // 1
         mouse?.mouseInput?.leftButton.pressedChangedHandler = { _, _, pressed in
           self.leftMouseDown = pressed
         }
-        // 2
-        mouse?.mouseInput?.mouseMovedHandler = { _, deltaX, deltaY in
-          self.mouseDelta = Point(x: deltaX, y: deltaY)
-        }
-        // 3
         mouse?.mouseInput?.scroll.valueChangedHandler = { _, xValue, yValue in
           self.mouseScroll.x = xValue
           self.mouseScroll.y = yValue
