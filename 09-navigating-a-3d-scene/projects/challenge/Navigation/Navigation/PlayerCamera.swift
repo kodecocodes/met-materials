3fc330c3fdddabb008a7fadf0f38efe47ef48969
Caroline Begbie <c.b100@aol.com>
2023-09-10 16:13:10 +1000

    Chapter 9 update

--- 09-navigating-a-3d-scene/projects/challenge/Navigation/Navigation/PlayerCamera.swift
+++ 09-navigating-a-3d-scene/projects/challenge/Navigation/Navigation/PlayerCamera.swift
@@ -1,12 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>FILEHEADER</key>
-	<string>/// Copyright (c) 2023 Kodeco Inc.
+///// Copyright (c) 2023 Kodeco Inc.
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the &quot;Software&quot;), to deal
+/// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
@@ -27,12 +22,54 @@
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
 ///
-/// THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.</string>
-</dict>
-</plist>
+/// THE SOFTWARE.
+
+import Foundation
+
+struct PlayerCamera: Camera {
+  var transform = Transform()
+  var aspect: Float = 1.0
+  var fov = Float(70).degreesToRadians
+  var near: Float = 0.1
+  var far: Float = 100
+  var projectionMatrix: float4x4 {
+    float4x4(
+      projectionFov: fov,
+      near: near,
+      far: far,
+      aspect: aspect)
+  }
+
+  mutating func update(size: CGSize) {
+    aspect = Float(size.width / size.height)
+  }
+
+  var viewMatrix: float4x4 {
+    let rotateMatrix = float4x4(
+      rotationYXZ: [-rotation.x, rotation.y, 0])
+    return (float4x4(translation: position) * rotateMatrix).inverse
+  }
+
+  mutating func update(deltaTime: Float) {
+    let transform = updateInput(deltaTime: deltaTime)
+    rotation += transform.rotation
+    position += transform.position
+    let input = InputController.shared
+    if input.leftMouseDown {
+      let sensitivity = Settings.mousePanSensitivity
+      rotation.x += input.mouseDelta.y * sensitivity
+      rotation.y += input.mouseDelta.x * sensitivity
+      rotation.x = max(-.pi / 2, min(rotation.x, .pi / 2))
+      input.mouseDelta = .zero
+    }
+
+  }
+}
+
+extension PlayerCamera: Movement { }
--- 09-navigating-a-3d-scene/projects/starter/Navigation/Navigation/Shaders/Fragment.metal
+++ 09-navigating-a-3d-scene/projects/starter/Navigation/Navigation/Shaders/Fragment.metal
@@ -1,12 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>FILEHEADER</key>
-	<string>/// Copyright (c) 2023 Kodeco Inc.
+///// Copyright (c) 2023 Kodeco Inc.
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the &quot;Software&quot;), to deal
+/// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
@@ -27,12 +22,31 @@
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
 ///
-/// THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.</string>
-</dict>
-</plist>
+/// THE SOFTWARE.
+
+#include <metal_stdlib>
+using namespace metal;
+#import "Common.h"
+#import "ShaderDefs.h"
+
+fragment float4 fragment_main(
+  constant Params &params [[buffer(ParamsBuffer)]],
+  VertexOut in [[stage_in]],
+  texture2d<float> baseColorTexture [[texture(BaseColor)]])
+{
+  constexpr sampler textureSampler(
+    filter::linear,
+    mip_filter::linear,
+    max_anisotropy(8),
+    address::repeat);
+  float3 baseColor = baseColorTexture.sample(
+    textureSampler,
+    in.uv * params.tiling).rgb;
+  return float4(baseColor, 1);
+}
