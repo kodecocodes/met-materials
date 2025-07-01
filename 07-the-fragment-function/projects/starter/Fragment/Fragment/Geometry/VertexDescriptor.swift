c16a038027c38f5f203f5f48a2812a1a94aa543f
Caroline <c.b100@aol.com>
2025-07-01 17:07:43 +1000

    Chapter 7 starter project

--- 07-the-fragment-function/projects/starter/Fragment/Fragment/Geometry/VertexDescriptor.swift
+++ 07-the-fragment-function/projects/starter/Fragment/Fragment/Geometry/VertexDescriptor.swift
@@ -1,4 +1,4 @@
-///// Copyright (c) 2023 Kodeco Inc.
+///// Copyright (c) 2025 Kodeco Inc.
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -32,33 +32,23 @@
 
 import MetalKit
 
-class Model: Transformable {
-  var transform = Transform()
-  let mesh: MTKMesh
-  let name: String
+extension MTLVertexDescriptor {
+  static var defaultLayout: MTLVertexDescriptor? {
+    MTKMetalVertexDescriptorFromModelIO(.defaultLayout)
+  }
+}
 
-  init(device: MTLDevice, name: String) {
-    guard let assetURL = Bundle.main.url(
-      forResource: name,
-      withExtension: nil) else {
-      fatalError("Model: \(name) not found")
-    }
-
-    let allocator = MTKMeshBufferAllocator(device: device)
-    let asset = MDLAsset(
-      url: assetURL,
-      vertexDescriptor: .defaultLayout,
-      bufferAllocator: allocator)
-    if let mdlMesh =
-      asset.childObjects(of: MDLMesh.self).first as? MDLMesh {
-      do {
-        mesh = try MTKMesh(mesh: mdlMesh, device: device)
-      } catch {
-        fatalError("Failed to load mesh")
-      }
-    } else {
-      fatalError("No mesh available")
-    }
-    self.name = name
+extension MDLVertexDescriptor {
+  static var defaultLayout: MDLVertexDescriptor {
+    let vertexDescriptor = MDLVertexDescriptor()
+    var offset = 0
+    vertexDescriptor.attributes[0] = MDLVertexAttribute(
+      name: MDLVertexAttributePosition,
+      format: .float3,
+      offset: 0,
+      bufferIndex: 0)
+    offset += MemoryLayout<float3>.stride
+    vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: offset)
+    return vertexDescriptor
   }
 }
